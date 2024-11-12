dc943af12278b47490c613d2af5199b19ba340da
JacksonKueny <jacksonkueny4@gmail.com>
2024-11-11 22:46:33 -0800

    Project

--- app/components/ProjectInfo.js
+++ app/components/ProjectInfo.js
@@ -7,6 +7,8 @@ function ProjectInfo() {
                 <a href="https://github.com/JacksonKueny/NextJS_w_React" target="_blank" rel="noopener noreferrer">
                     GitHub
                 </a>.
+                My project is about DDOS attacks and how to prevent them. For the project we plan to showcase this by explaining
+                what DDOS attacks are, the threats they pose, and some ways that they are prevented in the real world.
             </p>
         </div>
     );
